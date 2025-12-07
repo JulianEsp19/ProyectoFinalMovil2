658ce01b978659c06ea116f99ce3057675241a22
Julian Esparza <117664575+JulianEsp19@users.noreply.github.com>
2025-12-06 20:28:52 -0600

    Actualizacion de vista login
    se actualizo la vista del inicio de sesion dejando en cuanto a vista funcional

--- app/src/main/java/com/example/proyectofinalmovil3/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalmovil3/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.proyectofinalmovil3
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    //variables del entorno grafico
+    private lateinit var iniciarSesionButtonVista: Button
+    private lateinit var registrarseButtonVista: Button
+
+    private lateinit var iniciarSesionButton: Button
+    private lateinit var registrarseButton: Button
+
+    private lateinit var correoInicioSesionInput: TextInputLayout
+    private lateinit var contraseñaInicioSesionInput: TextInputLayout
+
+    private lateinit var nombreRegistroInput: TextInputLayout
+    private lateinit var correoRegistroInput: TextInputLayout
+    private lateinit var contraseñaRegistroInput: TextInputLayout
+
+    private lateinit var vistaInicioSesion: LinearLayout
+    private lateinit var vistaRegistro: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +38,33 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //inicializacion de variables del entorno grafico
+        iniciarSesionButtonVista = findViewById(R.id.iniciarSesionButtonVista2)
+        registrarseButtonVista = findViewById(R.id.registrarseButtonVista1)
+        iniciarSesionButton = findViewById(R.id.iniciarSesionButton)
+        registrarseButton = findViewById(R.id.registrarseButton)
+        correoInicioSesionInput = findViewById(R.id.correoInicioSesionInputLayout)
+        contraseñaInicioSesionInput = findViewById(R.id.contrasenaInicioSesionInputLayout)
+        nombreRegistroInput = findViewById(R.id.nombreRegistroInputLayout)
+        correoRegistroInput = findViewById(R.id.correoRegistroInputLayout)
+        contraseñaRegistroInput = findViewById(R.id.contrasenaRegistroInputLayout)
+        vistaInicioSesion = findViewById(R.id.inicioSesionLinearLayout)
+        vistaRegistro = findViewById(R.id.registroLinearLayout)
+
+        //acciones de botones
+        iniciarSesionButtonVista.setOnClickListener { vistaInicioSesion() }
+        registrarseButtonVista.setOnClickListener { vistaRegistro() }
+    }
+
+    private fun vistaRegistro(){
+        vistaInicioSesion.visibility = View.GONE
+        vistaRegistro.visibility = View.VISIBLE
+
+    }
+
+    private fun vistaInicioSesion(){
+        vistaInicioSesion.visibility = View.VISIBLE
+        vistaRegistro.visibility = View.GONE
     }
 }
