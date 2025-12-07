880d3bf904c5533e257547bea3d6cdae2b4757b9
Julian Esparza <117664575+JulianEsp19@users.noreply.github.com>
2025-12-06 22:35:14 -0600

    Main Funcional y correccion de color
    Se dejo el main funcional, a la espera de las siguientes vistas a la cual pasara, ademas de que se corrigio los colores de los inputTextLayout a color negro

--- app/src/main/java/com/example/proyectofinalmovil3/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalmovil3/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.proyectofinalmovil3
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +34,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var vistaInicioSesion: LinearLayout
     private lateinit var vistaRegistro: LinearLayout
 
+    //base de datos en firebase
+    private lateinit var database: DatabaseReference
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -38,6 +49,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+
+        comprobarInicioSesion()
 
         //inicializacion de variables del entorno grafico
         iniciarSesionButtonVista = findViewById(R.id.iniciarSesionButtonVista2)
@@ -52,9 +66,102 @@ class MainActivity : AppCompatActivity() {
         vistaInicioSesion = findViewById(R.id.inicioSesionLinearLayout)
         vistaRegistro = findViewById(R.id.registroLinearLayout)
 
+        //inicializacion de base de datos
+        database = Firebase.database.reference
+
         //acciones de botones
         iniciarSesionButtonVista.setOnClickListener { vistaInicioSesion() }
         registrarseButtonVista.setOnClickListener { vistaRegistro() }
+
+        iniciarSesionButton.setOnClickListener { iniciarSesion() }
+        registrarseButton.setOnClickListener { registrarse() }
+    }
+
+    private fun registrarse(){
+        val nombre = nombreRegistroInput.editText?.text.toString()
+        val correo = correoRegistroInput.editText?.text.toString()
+        val contraseña = contraseñaRegistroInput.editText?.text.toString()
+        if (comprobarCamposRegistro()) {
+            // Realizar la autenticación con Firebase
+            database.child("usuarios").child(correo).get().addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    // El usuario ya existe
+                    Toast.makeText(this, "El correo ya esta en uso", Toast.LENGTH_SHORT).show()
+                }else{
+                    //El usuario no existe
+                    database.child("usuarios").child(correo).child("nombre").setValue(nombre)
+                    database.child("usuarios").child(correo).child("contraseña").setValue(contraseña)
+                    Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+
+                    val editor = sharedPreferences.edit()
+                    editor.putString("email", correo)
+                    editor.putString("name",nombre)
+                    editor.apply()
+
+                    comprobarInicioSesion()
+                }
+            }
+        }
+    }
+
+    private fun iniciarSesion(){
+        val correo = correoInicioSesionInput.editText?.text.toString()
+        val contraseña = contraseñaInicioSesionInput.editText?.text.toString()
+        if (comprobarCamposInicioSesion()) {
+            // Realizar la autenticación con Firebase
+            database.child("usuarios").child(correo).get().addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    val contraseñaGuardada =
+                        dataSnapshot.child("contraseña").getValue(String::class.java)
+                    if (contraseña == contraseñaGuardada) {
+                        // Autenticación exitosa
+                        val name = dataSnapshot.child("nombre").getValue(String::class.java)
+
+                        //guardar datos de sesion para mantenerlos
+                        val editor = sharedPreferences.edit()
+                        editor.putString("email", correo)
+                        editor.putString("name",name)
+                        editor.apply()
+
+                        comprobarInicioSesion()
+                    } else {
+                        // Contraseña incorrecta
+                        Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun comprobarCamposInicioSesion(): Boolean {
+        val correo = correoInicioSesionInput.editText?.text.toString()
+        val contraseña = contraseñaInicioSesionInput.editText?.text.toString()
+        if (!correo.isEmpty() && !contraseña.isEmpty()) {
+            return true
+        }
+        return false
+    }
+
+    private fun comprobarCamposRegistro(): Boolean {
+        val nombre = nombreRegistroInput.editText?.text.toString()
+        val correo = correoRegistroInput.editText?.text.toString()
+        val contraseña = contraseñaRegistroInput.editText?.text.toString()
+        if (!nombre.isNullOrEmpty() && !correo.isNullOrEmpty() && !contraseña.isNullOrEmpty()) {
+            return true
+        }
+        return false
+    }
+
+
+    private fun comprobarInicioSesion(){
+        val savedEmail = sharedPreferences.getString("email", "")
+        val savedName = sharedPreferences.getString("name", "")
+        if (!savedName.isNullOrEmpty() || !savedEmail.isNullOrEmpty()) {
+            Toast.makeText(this, "Bienvenido $savedName", Toast.LENGTH_SHORT).show()
+            //val intent = Intent(this, MainActivity2::class.java)
+            //startActivity(intent)
+            finish()
+        }
     }
 
     private fun vistaRegistro(){
